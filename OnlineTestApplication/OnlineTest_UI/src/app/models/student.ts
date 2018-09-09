bf7c908fee82a8d52f5c9b3b000fb50e367763aa
vilasholkar <vilas.holkar29@gmail.com>
2018-09-09 20:28:10 +0530

    vp

---
+++ OnlineTestApplication/OnlineTest_UI/src/app/models/student.ts
@@ -0,0 +1,30 @@
+export interface StudentOnlineTest {
+    
+    StudentTestID: any;
+    StudentID: any;
+    OnlineTestID: any;
+    OnlineTestNo: any;
+    TestSeriesID: any;
+    TestSeriesName: any;
+    TestTypeID: any;
+    TestTypeName: any;
+    TestName: any;
+    TestDuration: any;
+    SessionID: any;
+    StreamID: any;
+    CourseID: any;
+    BatchID: any;
+    SubjectID: any;
+    Topic: any;
+    Instructions: any;
+    TestMarks: any;
+    PassingPercentage: any;
+    IsNegativeMarking: any;
+    StartDate: any;
+    StartTime: any;
+    EndDate: any;
+    EndTime: any;
+    IsVisible: any;
+    IsActive: any;
+    CreatedByUserID: any;
+}
