03e95fb20a20da8d79a555193bda9143c3c90d40
vilasholkar <vilas.holkar29@gmail.com>
2018-10-06 13:39:43 +0530

    VP

---
+++ OnlineTestApplication/OnlineTest_UI/src/app/models/result.ts
@@ -0,0 +1,31 @@
+export interface OT_Result
+{
+    ResultID: any; 
+    StudentID: any; 
+    StudentName: any; 
+    TestID: any; 
+    TestName: any; 
+    TestSeriesID: any; 
+    TestSeriesName: any; 
+    TestTypeID: any; 
+    TestTypeName: any; 
+    TestDate: any; 
+    Physics_Total: any; 
+    Physics_Right: any; 
+    Physics_Wrong: any; 
+    Chemistry_Total: any; 
+    Chemistry_Right: any; 
+    Chemistry_Wrong: any; 
+    Biology_Total: any; 
+    Biology_Right: any; 
+    Biology_Wrong: any; 
+    TotalCorrect: any; 
+    TotalWrong: any; 
+    TotalAttempt: any; 
+    TotalMarksObtained: any; 
+    Percentage: any; 
+    Rank: any; 
+    TotalMarks: any; 
+    QualifyingMarks: any; 
+    
+}
