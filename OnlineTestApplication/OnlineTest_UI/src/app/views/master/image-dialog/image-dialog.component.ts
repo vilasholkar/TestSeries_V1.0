fcc8d3551ad689b51ab87c5089f4e02d921c2ef3
vilasholkar <vilas.holkar29@gmail.com>
2019-02-17 17:15:05 +0530

    vp

---
+++ OnlineTestApplication/OnlineTest_UI/src/app/views/master/image-dialog/image-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-image-dialog',
+  templateUrl: './image-dialog.component.html',
+  styleUrls: ['./image-dialog.component.scss']
+})
+export class ImageDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+  }
+
+}
+export interface DialogData {
+  name: string;
+  image_url: string;
+}
