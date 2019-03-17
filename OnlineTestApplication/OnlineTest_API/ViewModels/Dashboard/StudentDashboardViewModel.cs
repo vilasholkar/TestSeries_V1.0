efa44b3df14d578e75ee188b4a16675b93c10783
vilasholkar <vilas.holkar29@gmail.com>
2019-03-17 19:06:13 +0530

    vp

---
+++ OnlineTestApplication/OnlineTest_API/ViewModels/Dashboard/StudentDashboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.Dashboard
+{
+   public class StudentDashboardViewModel
+    {
+        public int TotalTest { get; set; }
+        public int NotStarted { get; set; }
+        public int Started { get; set; }
+        public int Completed { get; set; }
+        public List<TestPercentage> TestPercentage { get; set; }
+    }
+
+    public class TestPercentage
+    {
+        public int ResultID { get; set; }
+        public string TestName { get; set; }
+        public string Percentage { get; set; }
+    }
+}
---
+++ OnlineTestApplication/OnlineTest_API/ViewModels/Result/StudentResponseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.Result
+{
+    public class StudentResponseViewModel
+    {
+        public int StudentResponseID { get; set; }
+        public int StudentID { get; set; }
+        public int TestID { get; set; }
+        public int QuestionID { get; set; }
+        public int SubjectID { get; set; }
+        public int OptionID { get; set; }
+        public int AnswerID { get; set; }
+        public bool IsCorrect { get; set; }
+        public string TestQuestionNo { get; set; }
+        public string Image_English { get; set; }
+        public string Image_Hindi { get; set; }
+
+    }
+}
